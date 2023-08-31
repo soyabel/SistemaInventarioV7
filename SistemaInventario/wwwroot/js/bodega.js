9fa29d78db55e6122f1ebd4df2f32cffa0e9579d
soyabel <soyabelca@gmail.com>
2023-08-30 22:59:20 -0500

    CRUD de Bodegas

---
+++ SistemaInventario/wwwroot/js/bodega.js
@@ -0,0 +1,92 @@
+﻿let datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    datatable = $('#tblDatos').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Bodega/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "nombre", "width": "20%" },
+            { "data": "descripcion", "width": "40%" },
+            {
+                "data": "estado",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Activo";
+                    }
+                    else {
+                        return "Inactivo";
+                    }
+                }, "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                           <a href="/Admin/Bodega/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                              <i class="bi bi-pencil-square"></i>  
+                           </a>
+                           <a onclick=Delete("/Admin/Bodega/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                <i class="bi bi-trash3-fill"></i>
+                           </a> 
+                        </div>
+                    `;
+                }, "width": "20%"
+            }
+        ]
+
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: '¿Esta seguro que deseas eliminar la Bodega?',
+        text: "Este registro no se podra recuperar!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si,eliminalo!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message)
+                        datatable.ajax.reload();
+                    } else {
+                        toastr.error(data.message)
+                    }
+                }
+
+            });
+            Swal.fire(
+                'Eliminado!',
+                'El registro fue eliminado exitosamente.',
+                'success'
+            )
+        }
+    })
+}
